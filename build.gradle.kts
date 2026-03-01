18c4e6f1b7d83652a4b8061fb63b873480570bb4
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2026-03-02 00:53:46 +0100

    feat: Shadow jar to export the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    id("com.gradleup.shadow") version "9.3.1"
 }
 
 group = "dev.tonimatas.myfriendlyrobot"
@@ -11,14 +12,24 @@ repositories {
 
 dependencies {
     implementation("com.github.kwhat:jnativehook:2.2.2")
+    implementation("net.java.dev.jna:jna:5.18.1")
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.jar {
+    
+    
+    dependsOn("shadowJar")
+    archiveClassifier.set("plain")
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+
     manifest {
         attributes(
             "Main-Class" to "dev.tonimatas.myfriendlyrobot.Main",
